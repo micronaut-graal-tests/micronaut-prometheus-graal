4018f376c02543208b0e0722323af8248fbe0fe7
graemerocher <graeme.rocher@gmail.com>
2019-10-10 16:04:36 +0200

    Use JFrog OSS

--- src/main/java/example/micronaut/AlarmController.java
+++ src/main/java/example/micronaut/AlarmController.java
@@ -1,5 +1,6 @@
 package example.micronaut;
 
+import io.micronaut.configuration.metrics.micrometer.annotation.MircometerTimed;
 import io.micrometer.core.annotation.Timed;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -11,12 +12,14 @@ import javax.inject.Singleton;
 @Controller("/")
 public class AlarmController {
 
+    @MircometerTimed
     @Timed(value = "method.alarms.save-single", percentiles = {0.5, 0.95}, histogram = true, description = "Save alarm timer (Single)")
     @Get("/alarm-single")
     public Single<Alarm> saveAlarmReactive() {
         return Single.just(new Alarm(1, "Alarm 1", "High"));
     }
 
+    @MircometerTimed
     @Timed(value = "method.alarms.save", percentiles = {0.5, 0.95}, histogram = true, description = "Save alarm timer")
     @Get("/alarm")
     public Alarm saveAlarm() {
